e08c1f61b4d08b73650724bf90c3b31ca6b5b488
Benoît <xalacar59@gmail.com>
2019-10-02 23:35:48 +0200

    Main Page template created

---
+++ FrontEnd/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCarouselSlide, MatCarouselSlideComponent } from '@ngmodule/material-carousel';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'FrontEnd';
+}
---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule} from './material-module';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    MatCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
