9c7576e16dac80f10d0ea1c9d80458ed4c01ba10
Benoît <xalacar59@gmail.com>
2020-01-08 03:34:26 +0100

    Deleting useless files

--- FrontEnd/src/app/models/genre.model.ts
+++
@@ -1,9 +0,0 @@
-import {Deserializable} from './deserializable.model';
-
-export class Genre implements Deserializable {
-  public genre: string;
-
-  deserialize(input: any): this {
-    return Object.assign(this, input);
-  }
-}
